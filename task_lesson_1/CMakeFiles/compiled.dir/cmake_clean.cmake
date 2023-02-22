6c6e9ccf4649a87dd9005cdafd70a8e513890a49
Gleb Kharitonov <g.xaritonov@bk.ru>
2023-02-22 14:26:40 +0300

    new_update

---
+++ task_lesson_1/CMakeFiles/compiled.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiled.dir/main.cpp.o"
+  "CMakeFiles/compiled.dir/main.cpp.o.d"
+  "compiled"
+  "compiled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
