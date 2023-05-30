0d3c4b613a5ebb9d827f9f6197e07a47ce1b1ef0
poluyan <svpoluyan@gmail.com>
2023-05-30 18:10:53 +0300

    subfind fix

--- addition_tasks/subfind/Subfind.cpp
+++ addition_tasks/subfind/Subfind.cpp
@@ -6,157 +6,199 @@
 
 using namespace std;
 
-size_t naive(const std::string &str, const std::string &sub) {
-    size_t n = str.size(), m = sub.size(), j = 0;
-    for (size_t i = 0; i < n - m + 1; i++) {
-        for (j = 0; j < m; j++) {
-            if (str[i + j] != sub[j])
-                break;
-        }
-        if (j == m)
-            return i;
-    }
-    return std::string::npos;
+size_t naive(const std::string &str, const std::string &sub)
+{
+	size_t n = str.size(), m = sub.size(), j = 0;
+	for(size_t i = 0; i < n - m + 1; i++)
+	{
+		for(j = 0; j < m; j++)
+		{
+			if(str[i + j] != sub[j])
+				break;
+		}
+		if(j == m)
+			return i;
+	}
+	return std::string::npos;
 }
 
 // https://e-maxx.ru/algo/prefix_function
 
 // https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
-size_t kmp(string &txt, const string &pat) {
-    int M = pat.size();
-    int N = txt.size();
+size_t kmp(string &txt, const string &pat)
+{
+	
+	
+	
+	int M = pat.size();
+	int N = txt.size();
 
-    vector<int> pi(M);
-    for (int i = 1; i < M; ++i) {
-        int j = pi[i - 1];
-        while (j > 0 && pat[i] != pat[j])
-            j = pi[j - 1];
-        if (pat[i] == pat[j]) ++j;
-        pi[i] = j;
-    }
+	int * pi = new int [M];
+	pi[0] = 0;
+	for(int i = 1; i < M; ++i)
+	{
+		int j = pi[i - 1];
+		while(j > 0 && pat[i] != pat[j])
+			j = pi[j - 1];
+		if(pat[i] == pat[j]) ++j;
+		pi[i] = j;
+	}
+	
+	for(int i = 0; i < M; i++)
+	{
+		std::cout << pi[i];
+	}
+	std::cout << std::endl;
 
-    int i = 0;
-    int j = 0;
-    while ((N - i) >= (M - j)) {
-        if (pat[j] == txt[i]) {
-            ++j;
-            ++i;
-        }
-        if (j == M) {
-            return i - j;
-        } else if (i < N && pat[j] != txt[i]) {
-            if (j != 0)
-                j = pi[j - 1];
-            else
-                ++i;
-        }
-    }
-    return std::string::npos;
+	int i = 0;
+	int j = 0;
+	//while ((N - i) >= (M - j)) {
+	/*if (pat[j] == txt[i]) {
+	    ++j;
+	    ++i;
+	}
+	if (j == M) {
+					delete [] pi;
+	    return i - j;
+	} else if (i < N && pat[j] != txt[i]) {
+	    if (j != 0)
+	        j = pi[j - 1];
+	    else
+	        ++i;
+	}*/
+	for(int i = 0; i < N; ++i)
+	{
+		while(j > 0 && txt[i] != pat[j])
+			j = pi[j - 1];
+		if(txt[i] == pat[j])
+			++j;
+		if(j == M)
+		{
+			delete [] pi;
+			return i - M + 1;
+		}
+	}
+	delete [] pi;
+	return std::string::npos;
 }
 
 // https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
-size_t bmSearch(const string &text, const string &pattern) {
-    int m = pattern.size();
-    int n = text.size();
+size_t bmSearch(const string &text, const string &pattern)
+{
+	int m = pattern.size();
+	int n = text.size();
 
-    int badchar[128];
-    int i;
-    for (i = 0; i < 128; i++)
-        badchar[i] = -1;
-    for (i = 0; i < m; i++)
-        badchar[(int) pattern[i]] = i;
+	int badchar[128];
+	int i;
+	for(i = 0; i < 128; i++)
+		badchar[i] = -1;
+	for(i = 0; i < m; i++)
+		badchar[(int) pattern[i]] = i;
 
-    int s = 0;
-    while (s <= (n - m)) {
-        int j = m - 1;
+	int s = 0;
+	while(s <= (n - m))
+	{
+		int j = m - 1;
 
-        while (j >= 0 && pattern[j] == text[s + j])
-            j--;
+		while(j >= 0 && pattern[j] == text[s + j])
+			j--;
 
-        if (j < 0) {
-            return s;
-        } else
-            s += max(1, j - badchar[text[s + j]]);
-    }
-    return std::string::npos;
+		if(j < 0)
+		{
+			return s;
+		}
+		else
+			s += max(1, j - badchar[text[s + j]]);
+	}
+	return std::string::npos;
 }
 
-int main() {
-    using namespace std;
+int main()
+{
+	using namespace std;
 
-    string str, sub = "was born in a small town called Sceptre";
+	string str, sub = "assists with 56 the following season";
 
-    string filename = "engwiki_ascii.txt";
-    ifstream fin(filename, ios::binary);
-    if (!fin.is_open()) {
-        cout << "not open! " << filename << endl;
-        return 0;
-    }
-    str.append((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
+	string filename = "engwiki_ascii.txt";
+	ifstream fin(filename, ios::binary);
+	if(!fin.is_open())
+	{
+		cout << "not open! " << filename << endl;
+		return 0;
+	}
+	str.append((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
 
-    size_t n = 10;
-    std::vector<size_t> times(n), indx(n);
+	size_t n = 5;
+	std::vector<size_t> times(n), indx(n);
 
-    cout << "\nstd::find\n";
-    for (size_t i = 0; i < n; i++) {
-        auto time_one = chrono::high_resolution_clock::now();
-        auto index = str.find(sub);
-        if (index == std::string::npos)
-            std::cout << "not found\n";
-        else
-            indx[i] = index;
-        auto time_two = chrono::high_resolution_clock::now();
-        times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
-    }
-    for (size_t i = 0; i < n; i++) {
-        cout << indx[i] << '\t' << times[i] << endl;
-    }
+	cout << "\nstd::find\n";
+	for(size_t i = 0; i < n; i++)
+	{
+		auto time_one = chrono::high_resolution_clock::now();
+		auto index = str.find(sub);
+		if(index == std::string::npos)
+			std::cout << "not found\n";
+		else
+			indx[i] = index;
+		auto time_two = chrono::high_resolution_clock::now();
+		times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
+	}
+	for(size_t i = 0; i < n; i++)
+	{
+		cout << indx[i] << '\t' << times[i] << endl;
+	}
 
-    cout << "\nnaive\n";
-    for (size_t i = 0; i < n; i++) {
-        auto time_one = chrono::high_resolution_clock::now();
-        auto index = naive(str, sub);
-        if (index == std::string::npos)
-            std::cout << "not found\n";
-        else
-            indx[i] = index;
-        auto time_two = chrono::high_resolution_clock::now();
-        times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
-    }
-    for (size_t i = 0; i < n; i++) {
-        cout << indx[i] << '\t' << times[i] << endl;
-    }
+	cout << "\nnaive\n";
+	for(size_t i = 0; i < n; i++)
+	{
+		auto time_one = chrono::high_resolution_clock::now();
+		auto index = naive(str, sub);
+		if(index == std::string::npos)
+			std::cout << "not found\n";
+		else
+			indx[i] = index;
+		auto time_two = chrono::high_resolution_clock::now();
+		times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
+	}
+	for(size_t i = 0; i < n; i++)
+	{
+		cout << indx[i] << '\t' << times[i] << endl;
+	}
 
-    cout << "\nKMP\n";
-    for (size_t i = 0; i < n; i++) {
-        auto time_one = chrono::high_resolution_clock::now();
-        auto index = kmp(str, sub);
-        if (index == std::string::npos)
-            std::cout << "not found\n";
-        else
-            indx[i] = index;
-        auto time_two = chrono::high_resolution_clock::now();
-        times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
-    }
-    for (size_t i = 0; i < n; i++) {
-        cout << indx[i] << '\t' << times[i] << endl;
-    }
+	cout << "\nKMP\n";
+	for(size_t i = 0; i < n; i++)
+	{
+		auto time_one = chrono::high_resolution_clock::now();
+		auto index = kmp(str, sub);
+		if(index == std::string::npos)
+			std::cout << "not found\n";
+		else
+			indx[i] = index;
+		auto time_two = chrono::high_resolution_clock::now();
+		times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
+	}
+	for(size_t i = 0; i < n; i++)
+	{
+		cout << indx[i] << '\t' << times[i] << endl;
+	}
 
-    cout << "\nBM\n";
-    for (size_t i = 0; i < n; i++) {
-        auto time_one = chrono::high_resolution_clock::now();
-        auto index = bmSearch(str, sub);
-        if (index == std::string::npos)
-            std::cout << "not found\n";
-        else
-            indx[i] = index;
-        auto time_two = chrono::high_resolution_clock::now();
-        times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
-    }
-    for (size_t i = 0; i < n; i++) {
-        cout << indx[i] << '\t' << times[i] << endl;
-    }
-    return 0;
+	cout << "\nBM\n";
+	for(size_t i = 0; i < n; i++)
+	{
+		auto time_one = chrono::high_resolution_clock::now();
+		auto index = bmSearch(str, sub);
+		if(index == std::string::npos)
+			std::cout << "not found\n";
+		else
+			indx[i] = index;
+		auto time_two = chrono::high_resolution_clock::now();
+		times[i] = chrono::duration_cast<chrono::milliseconds>(time_two - time_one).count();
+	}
+	for(size_t i = 0; i < n; i++)
+	{
+		cout << indx[i] << '\t' << times[i] << endl;
+	}
+	return 0;
 }
 
 /*
